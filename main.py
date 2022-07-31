1d1a429f3e6f4b4e3587b9a0939dc6f8ae8049ab
mahesh <maheshraju.9381@gmail.com>
2022-07-31 23:16:37 +0530

    Right paddel done

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
+import time
 from turtle import Turtle, Screen
 
 screen = Screen()
+screen.tracer(0)
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title('Pong Game')
+
+paddel = Turtle()
+paddel.shape('square')
+paddel.color('white')
+paddel.shapesize(stretch_wid=5, stretch_len=1)
+paddel.penup()
+
+
+paddel.goto(385, 0)
+
+
+
+def forward():
+    y = paddel.ycor() + 20
+    paddel.goto(paddel.xcor(), y)
+
+
+def backward():
+    y = paddel.ycor() - 20
+    paddel.goto(paddel.xcor(), y)
+
+
+screen.listen()
+screen.onkeypress(forward, "Up")
+screen.onkeypress(backward, "Down")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+
+
+
+screen.exitonclick()
